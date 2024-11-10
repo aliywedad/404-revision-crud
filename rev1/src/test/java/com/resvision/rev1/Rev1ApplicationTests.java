b3eaab06d4c02c6d852fb6c7ea80efcdfd74aa0f
aliywedad <aliysidahmedwedad@gmail.com>
2024-11-10 16:49:04 +0000

    back

---
+++ rev1/src/test/java/com/resvision/rev1/Rev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resvision.rev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
